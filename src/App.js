7beac0251273ad00d7fd13872c66c93e2f81f381
alphonseb <alphonse.bouy@gmail.com>
2020-02-09 20:52:37 +0100

    :sparkles: poc works

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios'
 import './App.css';
 
@@ -12,7 +12,77 @@ function App () {
     
     const [selectedAssetRate, setSelectedAssetRate] = useState(null);
     
+    const [selectedCondition, setSelectedCondition] = useState('');
+    const [selectedLimit, setSelectedLimit] = useState(0);
+    const [newAlertEmail, setNewAlertEmail] = useState('');
+    
+    const addAlert = () => {
+        const alert = {};
+        alert.email = newAlertEmail;
+        alert.emailSent = false;
+        alert.interval = window.setInterval(() => {
+            axios({
+                method: 'get',
+                url: `https://rest.coinapi.io/v1/exchangerate/${ selectedAsset.id }/USD`,
+                headers: {
+                    'X-CoinAPI-Key': 'D4AFF4EF-F2BF-429E-9042-C8D11C68C30D'
+                }
+            }).then(({ data }) => {
+                const condition = () => {
+                    switch (selectedCondition) {
+                        case 'inferior':
+                            return data.rate < selectedLimit;
+                        case 'superior':
+                            return data.rate > selectedLimit;
+                        default:
+                            return false;
+                    }
+                }
+                if (condition() && !alert.emailSent) {
+                    // send email
+                    console.log('email sent to :', alert.email);
+                    alert.emailSent = true
+                }
+            })
+        }, 1000*60);
+        setAlert(prevAlerts => [...prevAlerts, alert]);
+    }
+
+    
+    /**
+     * TRIED WEBSOCKET, BUT I AM NOT GETTING THE RESPONSES I WANT
+     */
+
+    // const stream = useRef(null);
+
+    // useEffect(() => {
+    //     stream.current = new WebSocket('wss://ws-sandbox.coinapi.io/v1/');
+    //     stream.current.addEventListener('open', () => {
+    //         stream.current.addEventListener('message', (response) => {
+    //             console.log(JSON.parse(response.data));
+    //         });
+    //     });
+    //     return () => {
+    //         stream.current.close();
+    //     };
+    // })
+
+    // const addAlert = () => {
+    //     console.log(selectedAsset);
+
+    //     stream.current.send(JSON.stringify({
+    //         type: 'hello',
+    //         apikey: 'D4AFF4EF-F2BF-429E-9042-C8D11C68C30D',
+    //         heartbeat: false,
+    //         subscribe_data_type: ['exrate'],
+    //         subscribe_filter_asset_id: [selectedAsset.id],
+    //         subscribe_filter_exchange_id: ['USD'],
+    //     }));
+    // };
+    
     useEffect(() => {
+        console.log('hello', assets);
+        
         axios({
             method: 'get',
             url: 'https://rest.coinapi.io/v1/assets',
@@ -28,7 +98,7 @@ function App () {
             })
             setAssets(newAssets);
         })
-    }, [assets]);
+    }, []);// eslint-disable-line react-hooks/exhaustive-deps
     
     const handleNewAlertTextChange = (e) => {
         setnewAlertText(e.target.value);
@@ -69,9 +139,17 @@ function App () {
         setSelectedAsset(selectedAsset);
     };
     
-    // const addAlert = () => {
-    //     const stream = new WebSocket()
-    // };
+    const handleLimitChange = ({ target }) => {
+        setSelectedLimit(target.value);
+    };
+    
+    const handleConditionChange = ({ target }) => {
+        setSelectedCondition(target.value);
+    };
+    
+    const handleEmailChange = ({ target }) => {
+        setNewAlertEmail(target.value);
+    };
     
     return (
         <div className="App">
@@ -85,7 +163,16 @@ function App () {
                 { selectedAssetRate && (
                     <>
                         <p>Rate of { selectedAsset.name } is at { selectedAssetRate } USD.</p>
-                        <button>Set up alert</button>
+                        <form action="#">
+                            <select value={ selectedCondition } name="condition" onChange={ handleConditionChange }>
+                                <option value="">Choose</option>
+                                <option value="inferior">&#60;</option>
+                                <option value="superior">&#62;</option>
+                            </select>
+                            <input type="email" name="email" value={ newAlertEmail } onChange={ handleEmailChange } />
+                            <input type="number" name="limit" value={ selectedLimit } onChange={ handleLimitChange } />
+                        </form>
+                        <button onClick={ addAlert }>Set up alert</button>
                     </>
                 ) }
                 <ul>
